972486f95f83bff0518090d7e49ab642950f82d1
larspLd <71321296+larspLd@users.noreply.github.com>
2022-01-22 22:03:52 +0200

    bb

--- Assets/Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Assets/Scripts/NewBehaviourScript.cs
@@ -7,6 +7,6 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Screen.SetResolution(6400, 1400, FullScreenMode.ExclusiveFullScreen);
+        Screen.SetResolution(200, 40, FullScreenMode.ExclusiveFullScreen);
     }
 }
